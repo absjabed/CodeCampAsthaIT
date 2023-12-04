3ccaf7fb4ef0c43f235070dca8257098156f8953
MD ABS Jabed <mabsjabed@asthait.com>
2023-12-04 19:22:05 +0600

    added mvc project code

---
+++ CodeCampMvc/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CodeCampMvc.Models;
+
+namespace CodeCampMvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _config;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    public IActionResult Index()
+    {
+            //https://OurSite.com/Home/Index/42
+        
+        var result = _config.GetValue<string>("IsProduction");
+        Console.WriteLine("Is it Production Environment: "+result);
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
